4e38a403904323669d2fdc6040bdbf10569b105e
skshihab27 <80275257+skshihab27@users.noreply.github.com>
2022-07-25 17:01:26 +0600

    Add files via upload

---
+++ XYZ SOLUTION/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="css/afterlog.css">
+<title>Home page</title>
+</head>
+<body>
+<div class="afterlog">
+<center>
+<h1>Welcome! to A-Z Solution</h1><hr>
+
+
+<div class="afterlog">
+
+<div class="sticky">
+<div class="topnav">
+
+  <a href="Admin/view/login.php">Admin</a>
+  <a href="customer/view/myform.php">Customer</a>
+  <a href="Manager/view/myform.php">Manager</a>
+  <a href="Vendor/view/myform.php">Vendor</a>
+
+</div>
+</div>
+<div class="log">
+</center>
+</div>
+</body>
+</html>
